f9ec9a88b4d62cadcd19d1d67668703ad9258e10
ajw28 <ajw28@st-andrews.ac.uk>
2015-02-02 12:09:38 +0000

    JUnit changed to JUnit4 format

--- src/test/java/uk/ac/standrews/cs/cs3099/useri/risk/game/GameEngineTest.java
+++ src/test/java/uk/ac/standrews/cs/cs3099/useri/risk/game/GameEngineTest.java
@@ -4,16 +4,25 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import uk.ac.standrews.cs.cs3099.useri.risk.clients.Client;
+
+import java.util.ArrayList;
 
 public class GameEngineTest {
+    GameEngine engine;
 
     @Before
     public void setUp() throws Exception {
-
+        engine = new GameEngine();
+        State state = new State();
+        ArrayList<Client> clients = new ArrayList<Client>();
+        engine.initialise(state, clients);
     }
 
     @Test
     public void testGameLoop() throws Exception {
-
+        fail();
+        //TODO need to test game loop!!!
+       // engine.gameLoop();
     }
 }
--- src/test/java/uk/ac/standrews/cs/cs3099/useri/risk/game/MapTest.java
+++ src/test/java/uk/ac/standrews/cs/cs3099/useri/risk/game/MapTest.java
@@ -1,7 +1,15 @@
 package uk.ac.standrews.cs.cs3099.useri.risk.game;
 
+import org.junit.Test;
 import static org.junit.Assert.*;
+
+
 
 public class MapTest {
 
+    @Test
+    public void canLoadDefaultMap(){
+        Map map = new Map();
+        assertTrue(map.getContinents() != null);
+    }
 }

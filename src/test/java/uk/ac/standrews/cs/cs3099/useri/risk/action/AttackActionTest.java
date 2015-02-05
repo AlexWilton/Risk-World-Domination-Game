14a137a686a3bb42510c553733682ddb23f7a8d0
AlexWilton <AlexWilton@users.noreply.github.com>
2015-02-05 18:25:23 +0000

    Example of how to write a test for testing an attack action

--- src/test/java/uk/ac/standrews/cs/cs3099/useri/risk/action/AttackActionTest.java
+++ src/test/java/uk/ac/standrews/cs/cs3099/useri/risk/action/AttackActionTest.java
@@ -1,24 +1,59 @@
 package uk.ac.standrews.cs.cs3099.useri.risk.action;
 
-import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
 import uk.ac.standrews.cs.cs3099.useri.risk.game.*;
-import uk.ac.standrews.cs.cs3099.useri.risk.helpers.TestGameStateFactory;
 
 import java.util.ArrayList;
 
-public class AttackActionTest extends TestCase {
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 
-    public void setUp() throws Exception {
-        super.setUp();
+public class AttackActionTest{
 
+    private State testState;
+    private CountrySet countries;
+    private Country indonesia, newGuinea, westernAustralia, easternAustralia;
+    private Player playerA, playerB;
+
+    @Before
+    public void setup(){
+        testState = new State();
+        ArrayList<Player> players = new ArrayList<Player>();
+        playerA = new Player(0,null);
+        playerB = new Player(1,null);
+        players.add(playerA);
+        players.add(playerB);
+        testState.setup(new Map(), players, null);
+        indonesia = testState.getCountryByID(38);
+        newGuinea = testState.getCountryByID(39);
+        westernAustralia = testState.getCountryByID(40);
+        easternAustralia = testState.getCountryByID(41);
     }
 
-    public void testValidateAgainstStatePass() throws Exception {
 
-    }
+    @Test
+    public void testAttackAction(){
+        //Test that Player A successfully conquers westernAustralia from Player B
+        playerA.addCountry(indonesia);
+        indonesia.setOwner(playerA);
+        indonesia.setTroops(3);
+        playerB.addCountry(westernAustralia);
+        westernAustralia.setOwner(playerB);
+        westernAustralia.setTroops(1);
 
-    public void testPerformOnState() throws Exception {
+        int[] playerADice = {6,6};
+        int[] playerBDice = {1};
+        AttackAction attack = new AttackAction(playerA, indonesia, westernAustralia, playerADice, playerBDice);
+        attack.performOnState(testState);
+
+        assertTrue(westernAustralia.getOwner().equals(playerA));
+        assertTrue(playerA.getOccupiedCountries().contains(westernAustralia));
+        assertFalse(playerB.getOccupiedCountries().contains(westernAustralia));
+        assertTrue(westernAustralia.getTroops() == 2);
+        assertTrue(indonesia.getTroops() == 1);
 
     }
 }

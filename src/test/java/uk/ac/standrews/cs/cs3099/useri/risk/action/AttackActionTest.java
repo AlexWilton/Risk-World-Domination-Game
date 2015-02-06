c5f6dd1bc918cc9ee8027d9834896c6ce7f82bef
ajw28 <ajw28@st-andrews.ac.uk>
2015-02-06 12:18:08 +0000

    More tests added

--- src/test/java/uk/ac/standrews/cs/cs3099/useri/risk/action/AttackActionTest.java
+++ src/test/java/uk/ac/standrews/cs/cs3099/useri/risk/action/AttackActionTest.java
@@ -21,22 +21,21 @@ public class AttackActionTest{
     @Before
     public void setup(){
         testState = new State();
+
+
         ArrayList<Player> players = new ArrayList<Player>();
         playerA = new Player(0,null);
         playerB = new Player(1,null);
         players.add(playerA);
         players.add(playerB);
         testState.setup(new Map(), players, null);
+        while(testState.getTurnStage() != TurnStage.STAGE_BATTLES) testState.nextStage(); //set turn stage to Battle
         indonesia = testState.getCountryByID(38);
         newGuinea = testState.getCountryByID(39);
         westernAustralia = testState.getCountryByID(40);
         easternAustralia = testState.getCountryByID(41);
-    }
 
-
-    @Test
-    public void testAttackAction(){
-        //Test that Player A successfully conquers westernAustralia from Player B
+        //PlayerA has indonesia with 3 armies and PlayerB has WesternAustralia with 1 army
         playerA.addCountry(indonesia);
         indonesia.setOwner(playerA);
         indonesia.setTroops(3);
@@ -44,9 +43,22 @@ public class AttackActionTest{
         westernAustralia.setOwner(playerB);
         westernAustralia.setTroops(1);
 
+        //PlayerA has NewGuinea with 1 army
+        playerA.addCountry(newGuinea);
+        newGuinea.setOwner(playerA);
+        newGuinea.setTroops(1);
+    }
+
+
+    @Test
+    public void testSuccessfulAttack(){
+        //Test that Player A successfully conquers westernAustralia from Player B
         int[] playerADice = {6,6};
         int[] playerBDice = {1};
         AttackAction attack = new AttackAction(playerA, indonesia, westernAustralia, playerADice, playerBDice);
+
+        assertTrue(attack.validateAgainstState(testState));
+
         attack.performOnState(testState);
 
         assertTrue(westernAustralia.getOwner().equals(playerA));
@@ -54,6 +66,43 @@ public class AttackActionTest{
         assertFalse(playerB.getOccupiedCountries().contains(westernAustralia));
         assertTrue(westernAustralia.getTroops() == 2);
         assertTrue(indonesia.getTroops() == 1);
-
     }
+
+    @Test
+    public void testUnSuccessfulAttack(){
+        //Test that Player A loses one army in indonesia when failing to take westernAustralia
+        int[] playerADice = {3,3};
+        int[] playerBDice = {3};
+        AttackAction attack = new AttackAction(playerA, indonesia, westernAustralia, playerADice, playerBDice);
+
+        assertTrue(attack.validateAgainstState(testState));
+
+        attack.performOnState(testState);
+
+        assertTrue(westernAustralia.getOwner().equals(playerB));
+        assertTrue(playerB.getOccupiedCountries().contains(westernAustralia));
+        assertFalse(playerA.getOccupiedCountries().contains(westernAustralia));
+        assertTrue(westernAustralia.getTroops() == 1);
+        assertTrue(indonesia.getTroops() == 2);
+    }
+
+    @Test
+    public void attemptInvalidMoves(){
+        int[] playerADice = {6,6,6}; // Attempt to use 3 dice. (PlayerA can only attack with a maximum of 2 armies!)
+        int[] playerBDice = {5};
+        AttackAction attack = new AttackAction(playerA, indonesia, westernAustralia, playerADice, playerBDice);
+        assertFalse(attack.validateAgainstState(testState));
+
+        playerADice = new int[]{6,6};
+        playerBDice = new int[]{}; //Player B defends with no dice. (Must defend with at least one army)
+        attack = new AttackAction(playerA, indonesia, westernAustralia, playerADice, playerBDice);
+        assertFalse(attack.validateAgainstState(testState));
+
+        //playerA cannot attack as he must leave his army in newGuinea and he only has one troop there.
+        playerADice = new int[]{6};
+        playerBDice = new int[]{6};
+        attack = new AttackAction(playerA, newGuinea, westernAustralia, playerADice, playerBDice);
+        assertFalse(attack.validateAgainstState(testState));
+    }
+
 }

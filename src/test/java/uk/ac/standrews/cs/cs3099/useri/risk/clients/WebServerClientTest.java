c3f288eb1dd4fafda776aa7ed7131515b6db24e3
ajw28 <ajw28@st-andrews.ac.uk>
2015-02-12 18:10:49 +0000

    Development on Backend for Web Client started

---
+++ src/test/java/uk/ac/standrews/cs/cs3099/useri/risk/clients/WebServerClientTest.java
@@ -0,0 +1,29 @@
+package uk.ac.standrews.cs.cs3099.useri.risk.clients;
+
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.standrews.cs.cs3099.useri.risk.clients.webClient.JettyServer;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class WebServerClientTest {
+    private JettyServer jettyServer;
+
+    @Before
+    public void setup(){
+        jettyServer = new JettyServer();
+    }
+
+    @Test
+    public void canServerStartAndStop(){
+        jettyServer.run();
+        assertTrue(jettyServer.isStarted());
+        try {
+            jettyServer.stop();
+        } catch (Exception e) {
+           fail();
+        }
+        assertTrue(jettyServer.isStopped());
+    }
+}

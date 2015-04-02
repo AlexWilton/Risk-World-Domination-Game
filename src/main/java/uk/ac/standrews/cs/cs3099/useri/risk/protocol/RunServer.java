0b8914da00366271ef50e154bf9650ccd4e7c217
bs44 <bs44@st-andrews.ac.uk>
2015-04-02 17:14:09 +0100

    Conform to the WebClient.

--- src/main/java/uk/ac/standrews/cs/cs3099/useri/risk/protocol/RunServer.java
+++ src/main/java/uk/ac/standrews/cs/cs3099/useri/risk/protocol/RunServer.java
@@ -1,9 +1,11 @@
 package uk.ac.standrews.cs.cs3099.useri.risk.protocol;
 
 
+import uk.ac.standrews.cs.cs3099.useri.risk.clients.WebClient;
+
 public class RunServer {
     public static void main(String[] args) {
-        ServerSocketHandler handler = new ServerSocketHandler(1234, 2, 30);
+        ServerSocketHandler handler = new ServerSocketHandler(1234, 2, new WebClient());
         handler.startServer(false);
     }
 }

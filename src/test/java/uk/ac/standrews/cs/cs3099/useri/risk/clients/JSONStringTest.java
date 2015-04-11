5cac2f3c70795bc4c5a6c7d97fb5370666f6fa0d
Ryo Yanagida <ry6@st-andrews.ac.uk>
2015-04-11 18:05:46 +0100

    updated Test to cover couple lines more

---
+++ src/test/java/uk/ac/standrews/cs/cs3099/useri/risk/clients/JSONStringTest.java
@@ -0,0 +1,34 @@
+package uk.ac.standrews.cs.cs3099.useri.risk.clients;
+
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.standrews.cs.cs3099.useri.risk.game.State;
+import uk.ac.standrews.cs.cs3099.useri.risk.helpers.TestGameStateFactory;
+
+/**
+ * Created by 120010447 on 11/04/15.
+ */
+
+import static org.junit.Assert.*;
+
+public class JSONStringTest {
+    State testState;
+
+    @Before
+    public void setup(){
+        testState = TestGameStateFactory.getTestGameState();
+    }
+
+
+    @Test
+    public void mapToJSONStringTest(){
+
+        assertTrue(false);
+    }
+
+    @Test
+    public void CountrytoJSONStringTest(){
+        assertTrue(false);
+    }
+
+}

5d57988e7973a7b6a53818d5cda394c64b70e92d
Ryo Yanagida <ry6@st-andrews.ac.uk>
2015-04-16 16:30:23 +0100

    bit of cleaning up and new test class for small unit tests

---
+++ src/test/java/uk/ac/standrews/cs/cs3099/useri/risk/game/MiscUnitTest.java
@@ -0,0 +1,20 @@
+package uk.ac.standrews.cs.cs3099.useri.risk.game;
+
+/**
+ * Created by 120010447 on 16/04/15.
+ */
+import org.junit.*;
+/**
+ * Test Class to do small unit tests on multiple misc classes
+ */
+public class MiscUnitTest {
+
+    RiskCard testCard;
+
+    @Test
+    public void testRiskCard(){
+        testCard = new RiskCard();
+
+    }
+
+}

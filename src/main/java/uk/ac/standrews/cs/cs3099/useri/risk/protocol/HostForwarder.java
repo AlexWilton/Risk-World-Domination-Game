4436d1da70d91931763a8061348c66bbf6c7f5a1
bs44 <bs44@st-andrews.ac.uk>
2015-04-16 17:41:47 +0100

    Javadoc added to HostForwarder, plus some refactoring there.

--- src/main/java/uk/ac/standrews/cs/cs3099/useri/risk/protocol/HostForwarder.java
+++ src/main/java/uk/ac/standrews/cs/cs3099/useri/risk/protocol/HostForwarder.java
@@ -17,7 +17,10 @@ import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 
 /**
- * Class only for forwarding messages from all clients to this client and vice versa.
+ * Class for forwarding messages and applying the command from a single client to the GameState.
+ * Includes methods to parse protocol commands and apply them to the state, and to get and send
+ * Diceroll-related commands. Also performs checking for timeouts when an acknowledgement or a
+ * move is required.
  */
 class HostForwarder {
     private static State state;
@@ -32,7 +35,6 @@ class HostForwarder {
     private boolean move_required;
 
     private double timer = System.currentTimeMillis();
-    private double diff;
     private int last_ack = 0;
 
     public HostForwarder(MessageQueue q, int move_timeout, int ack_timeout, int id, BufferedReader input) {
@@ -41,17 +43,31 @@ class HostForwarder {
         ACK_TIMEOUT = ack_timeout;
         ID = id;
         this.input = input;
-        diff = MOVE_TIMEOUT;
     }
 
-    public static void setState(State state) {
+    /**
+     * Sets the gamestate in this Forwarder.
+     * @param state GameState to be used.
+     */
+    protected static void setState(State state) {
         HostForwarder.state = state;
     }
 
+    /**
+     * Adds a Random Generator seed to this Forwarder.
+     * @param seed the seed is set to this value.
+     */
     public static void setSeed(RNGSeed seed) {
         HostForwarder.seed = seed;
     }
 
+    /**
+     * Waits on getting all the dicerolls from all the clients (this is only for one
+     * client respectively). Involves some busy waiting. Passes the Exceptions over.
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     */
     void getRolls() throws IOException, InterruptedException {
         Command comm = Command.parseCommand(input.readLine());
         messageQueue.sendAll(comm, ID);
@@ -72,12 +88,13 @@ class HostForwarder {
         RollNumberCommand roll = (RollNumberCommand) comm;
         String rollStr = roll.get("payload").toString();
         seed.addSeedComponent(rollStr, ID);
-
-        //seed = null;
     }
 
-    void playGame() throws IOException {
-        //System.err.println(state.getCurrentPlayer().getID() == state.getRolls().getID());
+    /**
+     * Forwards and applies commands. Throws exception up one level.
+     * @throws IOException
+     */
+    protected void playGame() throws IOException {
         while(true) {
             if (move_required && System.currentTimeMillis() > timer + MOVE_TIMEOUT) {
                 System.err.println("Mov from " + ID + " timed out");
@@ -102,6 +119,10 @@ class HostForwarder {
         }
     }
 
+    /**
+     * Check for acknowledgement, otherwise applies command to game state.
+     * @param comm the command to be checked.
+     */
     private void checkAckCases(Command comm) {
         if (comm instanceof AcknowledgementCommand) {
             if (((AcknowledgementCommand) comm).getAcknowledgementId() == last_ack) {
@@ -115,32 +136,36 @@ class HostForwarder {
         }
     }
 
-    private void processCommand(Command currentCommand) {
+    /**
+     * Processes game commands and applies them to game state.
+     * @param comm The command to be processed
+     */
+    private void processCommand(Command comm) {
         Player currentPlayer = state.getCurrentPlayer();
         ArrayList<Action> playerActions = new ArrayList<>();
-        if (currentCommand instanceof AttackCommand){
-            playerActions.add(processAttackCommand((AttackCommand) currentCommand));
+        if (comm instanceof AttackCommand){
+            playerActions.add(processAttackCommand((AttackCommand) comm));
         }
-        else if (currentCommand instanceof DeployCommand){
-            playerActions.addAll(processDeployCommand((DeployCommand) currentCommand));
+        else if (comm instanceof DeployCommand){
+            playerActions.addAll(processDeployCommand((DeployCommand) comm));
         }
-        else if (currentCommand instanceof FortifyCommand){
-            playerActions.add(processFortifyCommand((FortifyCommand) currentCommand));
+        else if (comm instanceof FortifyCommand){
+            playerActions.add(processFortifyCommand((FortifyCommand) comm));
         }
-        else if (currentCommand instanceof DrawCardCommand){
-            playerActions.add(processDrawCardCommand((DrawCardCommand) currentCommand));
+        else if (comm instanceof DrawCardCommand){
+            playerActions.add(processDrawCardCommand((DrawCardCommand) comm));
         }
-        else if (currentCommand instanceof SetupCommand){
-            playerActions.add(processSetupCommand((SetupCommand) currentCommand));
+        else if (comm instanceof SetupCommand){
+            playerActions.add(processSetupCommand((SetupCommand) comm));
         }
-        else if (currentCommand instanceof PlayCardsCommand){
-            playerActions.addAll(processPlayCardsCommand((PlayCardsCommand) currentCommand));
+        else if (comm instanceof PlayCardsCommand){
+            playerActions.addAll(processPlayCardsCommand((PlayCardsCommand) comm));
         }
-        else if (currentCommand instanceof DefendCommand){
-            processDefendCommand((DefendCommand) currentCommand);
+        else if (comm instanceof DefendCommand){
+            processDefendCommand((DefendCommand) comm);
         }
         else {
-            System.out.println("cant process command " + currentCommand.toJSONString());
+            System.out.println("cant process command " + comm.toJSONString());
             return;
         }
 
@@ -153,51 +178,44 @@ class HostForwarder {
             if (playerAction.validateAgainstState(state)) {
                 playerAction.performOnState(state);
             } else {
-                System.out.println("Error move did not validate: " + currentCommand);
+                System.out.println("Error move did not validate: " + comm);
                 System.exit(1);
             }
         }
 
         if(state.winConditionsMet()){
             Player winner = state.getWinner();
-
             System.out.println("Winner is " + winner.getID());
-            //TODO follow endGame protocol
             System.exit(0);
         }
         move_required = false;
-        //state.nextPlayer();
         timer = System.currentTimeMillis();
     }
 
-    private void processDefendCommand(DefendCommand currentCommand) {
-        state.getPlayer(ID).getClient().pushCommand(currentCommand);
+    /**
+     * Processes a defend command against the game state.
+     * @param comm The command to be processed
+     */
+    private void processDefendCommand(DefendCommand comm) {
+        state.getPlayer(ID).getClient().pushCommand(comm);
     }
 
+    /**
+     * Processes the Play_cards command of the protocol and creates a list of relevant actions.
+     * @param command The command to be parsed
+     * @return List of actions the command implies.
+     */
     private ArrayList<TradeAction> processPlayCardsCommand(PlayCardsCommand command){
-        /*{
-            "command": "play_cards",
-            "payload": {
-                "cards": [
-                    [1, 2, 3],
-                    [4, 5, 6]
-                ],
-                "armies": 3
-            },
-            "player_id": 0,
-            "ack_id": 1
-        }*/
-
         JSONObject payload = command.getPayload();
         int player = command.getPlayer();
         JSONArray cards = (JSONArray)(payload.get("cards"));
 
-        ArrayList<ArrayList<RiskCard>> triplets = new ArrayList<ArrayList<RiskCard>>();
+        ArrayList<ArrayList<RiskCard>> triplets = new ArrayList<>();
         for (Object tripletObject : cards) {
             JSONArray tripletJSON = (JSONArray) tripletObject;
-            ArrayList<RiskCard> triplet = new ArrayList<RiskCard>();
-            for (int i = 0; i<tripletJSON.size();i++){
-                int cardId = Integer.parseInt(tripletJSON.get(i).toString());
+            ArrayList<RiskCard> triplet = new ArrayList<>();
+            for (Object aTripletJSON : tripletJSON) {
+                int cardId = Integer.parseInt(aTripletJSON.toString());
                 triplet.add(state.getPlayers().get(player).getRiskCardById(cardId));
             }
             triplets.add(triplet);
@@ -212,20 +230,18 @@ class HostForwarder {
     }
 
 
+    /**
+     * Processes an Attack Command and retrieves the defend action from the defender as well.
+     * @param command The attack command to be parsed
+     * @return The AttackAction created from this command.
+     */
     private AttackAction processAttackCommand(AttackCommand command){
-        /*{
-            "command": "attack",
-            "payload": [1,2,1],
-            "player_id" : 1
-        }*/
-
         JSONArray attackPlan = command.getPayloadAsArray();
         int player = command.getPlayer();
         int originId = Integer.parseInt(attackPlan.get(0).toString());
         int objectiveId = Integer.parseInt(attackPlan.get(1).toString());
         int attackArmies = Integer.parseInt(attackPlan.get(2).toString());
 
-        // TODO Need to pull the defendCommand from somewhere, but how?
         // Now if a DefendCommand arrives then we need Stuff.
         DefendCommand def = state.getCountryByID(objectiveId).getOwner().getClient().popDefendCommand(originId, objectiveId, attackArmies);
         int defendArmies = def.getPayloadAsInt();
@@ -248,16 +264,12 @@ class HostForwarder {
         return new AttackAction(state.getPlayer(player),state.getCountryByID(originId),state.getCountryByID(objectiveId),attackDice,defendDice);
     }
 
+    /**
+     * Processes a fortification request
+     * @param command the command to be parsed
+     * @return Action to be taken by the player.
+     */
     private FortifyAction processFortifyCommand(FortifyCommand command){
-        /*
-        {
-            "command": "fortify",
-            "payload": [1, 2, 5],
-            "player_id": 0,
-            "ack_id": 1
-        }
-        */
-
         if (command.get("payload") == null){
             return new FortifyAction(state.getPlayer(command.getPlayer()));
         }
@@ -267,57 +279,63 @@ class HostForwarder {
         int objectiveId = Integer.parseInt(fortification.get(1).toString());
         int armies = Integer.parseInt(fortification.get(2).toString());
 
-        FortifyAction act = new FortifyAction(state.getPlayers().get(player),state.getCountryByID(originId),state.getCountryByID(objectiveId),armies);
-        System.out.println("Interpreted fortify command");
-
-        return act;
-
-
+        return new FortifyAction(state.getPlayers().get(player),state.getCountryByID(originId),state.getCountryByID(objectiveId),armies);
     }
 
+    /**
+     * Processes the command for drawing a risk card.
+     * @param command the command to be parsed
+     * @return the action corresponding to the command.
+     */
     private ObtainRiskCardAction processDrawCardCommand(DrawCardCommand command){
-        /*{
-            "command": "draw_card",
-            "payload": 12,
-            "player_id": 0,
-            "ack_id": 1
-        }*/
-
         int player = command.getPlayer();
         ObtainRiskCardAction act = new ObtainRiskCardAction(state.getPlayers().get(player));
         System.out.println("Interpreted draw command");
         return act;
     }
 
+    /**
+     * Processes an army deploy command
+     * @param command the command to be processed
+     * @return the list of actions corresponding to the command
+     */
     private ArrayList<DeployArmyAction> processDeployCommand(DeployCommand command) {
         ArrayList<DeployArmyAction> deployArmyActions = new ArrayList<>();
         JSONArray territoryNumberPairs = command.getPayloadAsArray();
         int player = command.getPlayer();
-        //construct amount of deploy actions
+        //construct list of deploy actions
         for (Object pair : territoryNumberPairs) {
             //extract parameters
             JSONArray pairArray = (JSONArray) pair;
             int countryId = Integer.parseInt(pairArray.get(0).toString());
             int armies = Integer.parseInt(pairArray.get(1).toString());
 
-            //create deploy action and add it to action list
             deployArmyActions.add(new DeployArmyAction(state.getPlayer(player),state.getCountryByID(countryId),armies));
         }
         return deployArmyActions;
     }
 
+    /**
+     * Processes setup command at the beginning of the game.
+     * @param command command to be processed
+     * @return Relevant setup action.
+     */
     private SetupAction processSetupCommand(SetupCommand command){
         int countryId = command.getPayloadAsInt();
         return new SetupAction(state.getPlayer(command.getPlayer()),state.getCountryByID(countryId));
     }
 
-    public void signalAck(int ack_id) {
+    /**
+     * Signals this thread that an acknowledgement is required by this client.
+     * @param ack_id The ack_id of the acknowledgement expected.
+     */
+    protected void signalAck(int ack_id) {
         last_ack = ack_id;
         timer = System.currentTimeMillis();
         ack_received = false;
     }
 
-    boolean hasNoSeed() {
+    protected boolean hasNoSeed() {
         return seed == null;
     }
 }

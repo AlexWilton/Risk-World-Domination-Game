a5099655c3465f5839433f5f08795c91741d7f6c
Ryo Yanagida <yryo617@gmail.com>
2015-04-17 15:40:23 +0100

    Empty AI barebone

---
+++ src/main/java/uk/ac/standrews/cs/cs3099/useri/risk/clients/ai/AI.java
@@ -0,0 +1,19 @@
+package uk.ac.standrews.cs.cs3099.useri.risk.clients.ai;
+
+import uk.ac.standrews.cs.cs3099.useri.risk.clients.Client;
+import uk.ac.standrews.cs.cs3099.useri.risk.game.State;
+
+/**
+ * Created by ryo_yanagida on 17/04/2015.
+ */
+public abstract class AI extends Client {
+
+
+    public AI(State gamestate) {
+        super(gamestate);
+    }
+
+
+
+
+}

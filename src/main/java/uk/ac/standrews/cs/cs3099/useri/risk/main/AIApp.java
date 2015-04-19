9506afc96cc4381c4d6022c8b6a0a7b6bf00e632
Patrick Opgenoorth <po26@st-andrews.ac.uk>
2015-04-19 17:27:46 +0100

    Add AIRunner

--- src/main/java/uk/ac/standrews/cs/cs3099/useri/risk/main/AIApp.java
+++ src/main/java/uk/ac/standrews/cs/cs3099/useri/risk/main/AIApp.java
@@ -3,8 +3,11 @@ package uk.ac.standrews.cs.cs3099.useri.risk.main;
 import uk.ac.standrews.cs.cs3099.useri.risk.clients.AI.BulldogAIClient;
 import uk.ac.standrews.cs.cs3099.useri.risk.clients.AI.BulldogAIv2Client;
 import uk.ac.standrews.cs.cs3099.useri.risk.clients.AI.RandomAIClient;
+import uk.ac.standrews.cs.cs3099.useri.risk.clients.Client;
 import uk.ac.standrews.cs.cs3099.useri.risk.game.GameEngine;
 import uk.ac.standrews.cs.cs3099.useri.risk.helpers.ClientSocketHandler;
+
+import java.util.ArrayList;
 
 /**
  * Created by patrick on 17/04/15.
@@ -13,22 +16,21 @@ public class AIApp {
 
 
     public static void main(String argv[]){
-        ClientSocketHandler socketHandler = new ClientSocketHandler();
-        float[] versions = {1};
-        String[] features = {};
+        Client[] clients = {new BulldogAIv2Client(null),new RandomAIClient(null),new RandomAIClient(null),new RandomAIClient(null),new RandomAIClient(null),new RandomAIClient(null)};
+        String[] names = {"bulldog","random","random","random","random","random"};
+        ArrayList<Thread> threads = new ArrayList<>();
+        for (int i = 0; i<clients.length; i ++){
+            Thread t = new Thread(new AIRunner(clients[i],names[i]));
+            t.start();
+            threads.add(t);
+        }
 
-        //connect and obtain game information
-        int result = socketHandler.initialise("127.0.0.1",8888,new BulldogAIv2Client(null),versions,features,"b");
-
-        if(result == ClientApp.SUCCESS) {
-            //Run the socket handler
-            Thread socketHandlerThread = new Thread(socketHandler);
-            socketHandlerThread.start();
-
-            //initialise and start the game engine thread
-            GameEngine engine = new GameEngine(socketHandler);
-            Thread gameEngineThread = new Thread(engine);
-            gameEngineThread.start();
+        for (Thread t : threads){
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
 
